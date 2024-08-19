7eccf668075c9dc27e0014fcc012e361eacd3b00
GlittersIsGold <cyberk1ra@yandex.ru>
2024-08-19 18:27:10 +0300

    api interaction logic

--- api/client.go
+++ api/client.go
@@ -1,1 +1,30 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+const baseUrl = "https://rdb.altlinux.org/api/export/branch_binary_packages/"
+
+type Package struct {
+	Name	string `json:"name"`
+	Version string `json:"version"`
+	Release string `json:"release"`
+	Arch 	string `json:"arch"`
+}
+
+func FetchPackages(branch string) (map[string][]Package, error) {
+	res, err := http.Get(baseUrl + branch)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var packages map[string][]Package
+	if err := json.NewDecoder(res.Body).Decode(&packages); err != nil {
+		return nil, err
+	} 
+
+	return packages, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GlittersIsGold/altpkgdiff
+
+go 1.22.0
